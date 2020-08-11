f677cba53406eee9f2635127b0e6bed2e247e044
alienx456 <estebanelias27dev@gmail.com>
2020-08-11 17:42:17 -0500

    Interfaz

---
+++ src/app/interfaces/salida.ts
@@ -0,0 +1,10 @@
+export interface Salida {
+    vuelo: String,
+    fecha: Date,
+    retraso_horas: Number,
+    destino_ciudad: String,
+    internacional: Boolean,
+    aerolinea: String,
+    pasajeros: Number,
+    avion: String
+}
