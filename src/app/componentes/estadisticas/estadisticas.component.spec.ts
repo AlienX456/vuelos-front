0088d411971ba955d979c0e4c13b5e5d6bb6e456
alienx456 <estebanelias27dev@gmail.com>
2020-08-11 17:28:00 -0500

    Estadisticas finalizado

--- src/app/componentes/estadisticas/estadisticas.component.spec.ts
+++ src/app/componentes/estadisticas/estadisticas.component.spec.ts
@@ -33,17 +33,18 @@ describe('EstadisticasComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('Verificar que la variable estadisticas obtenga valor luego del llamado de la función de busqueda', fakeAsync(() => {
 
   const mockStat =
-    {
-      num_vuelos: 3,
-      num_retrasos: 3,
-      sum_retrasos: 12,
-      prom_pasajero: 106.66666666666667,
-      sum_pasajeros: 320,
-      num_vuelos_internacionales: 3
+  {
+    num_vuelos: 3,
+    num_retrasos: 3,
+    sum_retrasos: 12,
+    prom_pasajero: 106.66666666666667,
+    sum_pasajeros: 320,
+    num_vuelos_internacionales: 3
   };
+
+  it('Verificar que la variable estadisticas obtenga valor luego del llamado de la función de busqueda', fakeAsync(() => {
 
   spyOn(service, 'obtenerEstadisticas').and.returnValue(of(mockStat));
   fixture.componentInstance.selectedValue = "salidas";
@@ -56,5 +57,16 @@ describe('EstadisticasComponent', () => {
                                   expect(fixture.componentInstance.estadistica.num_vuelos_internacionales).toEqual(mockStat.num_vuelos_internacionales)});
 
   }))
+
+  it('Verificar que los valores de la variable entidad son reemplazados en tabla de forma correcta', () => {
+    fixture.componentInstance.estadistica = mockStat;
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css('.v-1')).nativeElement.textContent).toEqual(mockStat.num_vuelos.toString());
+    expect(fixture.debugElement.query(By.css('.v-2')).nativeElement.textContent).toEqual(mockStat.num_retrasos.toString());
+    expect(fixture.debugElement.query(By.css('.v-3')).nativeElement.textContent).toEqual(mockStat.sum_retrasos.toString());
+    expect(fixture.debugElement.query(By.css('.v-4')).nativeElement.textContent).toEqual(mockStat.prom_pasajero.toString());
+    expect(fixture.debugElement.query(By.css('.v-5')).nativeElement.textContent).toEqual(mockStat.sum_pasajeros.toString());
+    expect(fixture.debugElement.query(By.css('.v-6')).nativeElement.textContent).toEqual(mockStat.num_vuelos_internacionales.toString());
+  })
 
 });
